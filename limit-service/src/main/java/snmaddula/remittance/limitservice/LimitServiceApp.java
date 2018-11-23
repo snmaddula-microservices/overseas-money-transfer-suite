0246fef12990fa4afdca8a626160b4706ef91707
snmaddula <sn_maddula@outlook.com>
2018-11-23 12:30:40 +0530

    wired swagger

--- limit-service/src/main/java/snmaddula/remittance/limitservice/LimitServiceApp.java
+++ limit-service/src/main/java/snmaddula/remittance/limitservice/LimitServiceApp.java
@@ -3,12 +3,15 @@ package snmaddula.remittance.limitservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import snmaddula.components.swaggerready.SwaggerReady;
+
 /**
  * Remittance Limit Service Application
  * 
  * @author snmaddula
  *
  */
+@SwaggerReady
 @SpringBootApplication
 public class LimitServiceApp {
 
