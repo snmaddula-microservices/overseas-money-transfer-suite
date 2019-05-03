1bf1ae31d20e77e13c2c8fefcd2e0a67a7409638
snmaddula <snmaddula@gmail.com>
2019-05-03 14:41:24 +0530

    progress on limit-service

--- limits-service/src/main/java/snmaddula/remittance/limitsservice/LimitsApp.java
+++ limits-service/src/main/java/snmaddula/remittance/limitsservice/LimitsApp.java
@@ -3,11 +3,16 @@ package snmaddula.remittance.limitsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author snmaddula
+ *
+ */
 @SpringBootApplication
-public class LimitsServiceApplication {
+public class LimitsApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LimitsServiceApplication.class, args);
+		SpringApplication.run(LimitsApp.class, args);
 	}
 
 }
