d516afd31dbf94119595e8f47587235155d0404d
snmaddula <snmaddula@gmail.com>
2019-05-06 10:56:31 +0530

    Added Discovery Client & Hystrix

--- limits-service/src/main/java/snmaddula/remittance/limit/LimitsApp.java
+++ limits-service/src/main/java/snmaddula/remittance/limit/LimitsApp.java
@@ -1,18 +1,30 @@
-package snmaddula.remittance.limitsservice;
+package snmaddula.remittance.limit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
+ * The entry point class for Remittance Limit Service
  * 
  * @author snmaddula
  *
  */
+@EnableHystrix
+@EnableDiscoveryClient
 @SpringBootApplication
 public class LimitsApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LimitsApp.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
