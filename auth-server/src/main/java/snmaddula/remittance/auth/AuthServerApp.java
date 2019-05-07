3b01de3e454602ae9f0cafaa73adae1d3b43f633
snmaddula <snmaddula@gmail.com>
2019-05-07 12:37:57 +0530

    auth-server initial commit

---
+++ auth-server/src/main/java/snmaddula/remittance/auth/AuthServerApp.java
@@ -0,0 +1,18 @@
+package snmaddula.remittance.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author snmaddula
+ *
+ */
+@SpringBootApplication
+public class AuthServerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerApp.class, args);
+	}
+
+}
